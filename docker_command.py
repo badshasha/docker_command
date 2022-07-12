adbcaa0191470711645281c593edec9679d7e7ca
badshasha <shavendragoesoft@gmail.com>
2022-07-12 09:38:48 +0530

    stop & kill

--- docker_command.py
+++ docker_command.py
@@ -1,3 +1,17 @@
 docker logs a17666613276bc2fc947c38f04c5033dd8f85007979ab33277ae612236e5a4c3
 # this command not re running or execute the program 
 # it's just showing information form log information of previous run 
+
+
+# stap commandd
+docker stop id  # send to the pirmary process [ SIGTERM ]
+                # this thing is safe [ internal processor can do additional thing like safe time , save point  ]
+
+                # nearly takes 10 secound 
+
+docker kill id  # SIGKILL -> immediate kill the process 
+                # instance do it 
+
+
+# speialci not 
+#  but if process not working for stop it's automatically change to docker kill command 
