c550bb2fd42f22b45783bc9258ae792311873ce7
badshasha <shavendragoesoft@gmail.com>
2022-07-12 08:24:08 +0530

    docker create and start

--- docker_command.py
+++ docker_command.py
@@ -1,2 +1,15 @@
 docker logout
 docker login
+
+---------------------------------
+
+# create and start command 
+
+docker create hello-world  # output ==> fcde4a9d0f49e941d4e10e931cfff5f485f2a5202665ba89913738afb9a7632b  [id of the container]
+docker strat -a fcde4a9d0f49e941d4e10e931cfff5f485f2a5202665ba89913738afb9a7632b  # output ==> run the container
+
+# -a is the run command for container without it , it's only print the ID
+
+# there is a different between run and start :
+    # run printed all the terminal output + progam output  
+    # start olny pring progamm output 
