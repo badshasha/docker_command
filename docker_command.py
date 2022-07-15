c3b3e125f57101469121fc2886d568abd7a2065b
badshasha <shavendragoesoft@gmail.com>
2022-07-15 10:51:14 +0530

    docker volumes

--- docker_command.py
+++ docker_command.py
@@ -13,3 +13,12 @@ docker-compose down
 
 # build docker file with docker file name
 docker build -f dockerfile.dev .
+
+
+# docker wtih volum
+
+docker run -p 3000:3000 -v $(pws):/app imagename
+
+# docker with working volumns 
+
+docker run -p 3000:3000 -v /app/node_modules -v $(pws):/app imagename
